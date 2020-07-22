fca8617b158b21613f4d6646f66ec4efbdd61cb3
Daniel Maděra <madera.dan@gmail.com>
2020-07-22 23:23:12 +0200

    Add no cache to sheets and js

--- admin/layout/header.php
+++ admin/layout/header.php
@@ -4,9 +4,9 @@
     <title>LED Reklama Sněhulák</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='../assets/bootstrap-4.5.0-dist/css/bootstrap.min.css'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css?time=<?php echo time(); ?>'>
     <script src="../assets/jquery-3.5.1.min.js"></script>
     <script src="../assets/popper.min.js"></script>
     <script src="../assets/bootstrap-4.5.0-dist/js/bootstrap.min.js"></script>
-    <script src="main.js"></script>
+    <script src="main.js?time=<?php echo time(); ?>"></script>
 </head>
